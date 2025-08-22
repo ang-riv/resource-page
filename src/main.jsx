7f6c510bcdfb1c5c28678d4109aaa380f9c2523c
a.rivera <a.riveraa99@gmail.com>
2025-08-22 18:43:48 -0500

    Remove old import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import TestSpace from "./TestSpace.jsx";
 import { ResourceProvider } from "./ResourceContext.jsx";
 
 createRoot(document.getElementById("root")).render(
